f3ab96b3dc0366d763f52a223b5fb3a78f5d7434
Nina Katarina Weiss <55079647+NinaWeiss@users.noreply.github.com>
2020-02-18 08:14:30 +0200

    Create Main.java

---
+++ w02_klass/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+    
+    
+    public static void main(String[] args) {
+
+        Human one = new Human("Nina","female",24,"brown","green",1.81,"white");
+        Human two = new Human("Hannah","female",23,"blonde","blue",1.75,"white");
+        Human three= new Human("Laurence","male",12,"red","brown",1.20,"black");
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+    }
+}
